c2b22786423a2157e1a19ac7490834d1ea37d9b7
Shaimaa-moh <19P7484@eng.asu.edu.eg>
2022-06-06 21:10:18 +0200

    Update StudentList.java

--- src/main/Main_App/Main_App/StudentList.java
+++ src/main/Main_App/Main_App/StudentList.java
@@ -1,6 +1,16 @@
 package Main_App;
 
 public class StudentList {
+<<<<<<< HEAD
     String arr[][]={{"19p7484","database"},{"19p1881","compilers"}};
 
+=======
+    ArrayList<String> arr = new ArrayList<String>() {
+        {
+            add("https://engasuedu.sharepoint.com/sites/CSE371UG2018-ControlEngineering24668");
+            add("www.google.com");
+            add("www.youtube.com");
+        }
+    };
+>>>>>>> parent of 1b9d68e (added studentList class and try testing)
 }
