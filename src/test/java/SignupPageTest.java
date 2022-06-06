19015ac0fd4f7ee2dd794d556918009a8b38c18e
Omar Mohamed <mourra24@gmail.com>
2022-06-06 17:25:42 +0200

    GUI testing

--- src/test/java/SignupPageTest.java
+++ src/test/java/SignupPageTest.java
@@ -20,24 +20,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(ApplicationExtension.class)
 class SignupPageTest {
-
+    public Stage temp;
     @Start
     private void start(Stage stage) throws IOException {
         stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("SignupPage.fxml")))));
+        temp=stage;
         stage.show();
     }
 
     @Test
     void signUpPage(FxRobot robot) throws InterruptedException {
         robot.interact(() -> robot.lookup("#Email").queryAs(TextField.class).setText("test12"));
-        Thread.sleep(100);
+        Thread.sleep(1000);
         robot.interact(() -> robot.lookup("#Phone").queryAs(TextField.class).setText("test12"));
-        Thread.sleep(10000);
+        Thread.sleep(1000);
         robot.interact(() -> robot.lookup("#Name").queryAs(TextField.class).setText("test12"));
-        Thread.sleep(100);
+        Thread.sleep(1000);
         robot.interact(() -> robot.lookup("#Password").queryAs(TextField.class).setText("test12"));
-        Thread.sleep(100);
-        robot.clickOn("#SignUpButton");
+        Thread.sleep(1000);
+        robot.clickOn("#BackButton");
+
+
 
 
     }
