04f85145f610e766c134004c0ac9886b6e847eb6
Shaimaa-moh <19P7484@eng.asu.edu.eg>
2022-06-06 20:54:17 +0200

    Update StudentList.java

--- src/main/Main_App/Main_App/StudentList.java
+++ src/main/Main_App/Main_App/StudentList.java
@@ -1,14 +1,6 @@
 package Main_App;
 
-import java.util.ArrayList;
-
 public class StudentList {
-
-    String[][] arr1 = {{"19p1881","database"},{"19p2468","AI"},{"19p2057","compilers"}};
-    int rows=arr1.length;
-    for(int i=0;i<rows;i++)
-    {
-
-    }
-
+    String arr[][]={{"19p7484","database"},{"19p1881","compilers"}};
+    
 }
